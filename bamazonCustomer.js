9e0e3737c330eee82e1f32774d4aed9e5b81b3df
elcid00 <elcid00@aol.com>
2019-03-31 11:58:30 -0400

    initial prompt works

---
+++ bamazonCustomer.js
@@ -0,0 +1,40 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazonDB"
+  });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    runSearch();
+});
+  
+console.log("\nWelcome to Bamazon!\n")
+
+  function runSearch() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+          "See list of products for sale.",
+          "Exit"
+        ]
+      })
+      .then(function(answer){
+          switch(answer.action) {
+              case "See list of products for sale.":
+              productListSearch();
+              break;
+              case "Exit":
+              exit();
+              break;
+          }
+      });
+}
